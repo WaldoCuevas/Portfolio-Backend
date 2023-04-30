6384b5509d5389146202745b8fe7a3230614a099
WaldoCuevas <104439067+WaldoCuevas@users.noreply.github.com>
2023-04-30 19:06:28 -0300

    Unnecesary Repository

--- src/main/java/com/portfolio/portfolioBackend/Security/Repository/RolRepository.java
+++ src/main/java/com/portfolio/portfolioBackend/Security/Repository/RolRepository.java
@@ -3,13 +3,11 @@ package com.portfolio.portfolioBackend.Security.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.portfolio.portfolioBackend.Security.Enums.RolNombre;
 import com.portfolio.portfolioBackend.Security.Models.Rol;
 
-@Repository
-public interface RolRepository  extends JpaRepository<Rol,Integer>{
+public interface RolRepository extends JpaRepository<Rol,Integer>{
  
     Optional<Rol> findByRolNombre(RolNombre rolNombre);
 
--- src/main/java/com/portfolio/portfolioBackend/Security/Repository/UsuarioRepository.java
+++ src/main/java/com/portfolio/portfolioBackend/Security/Repository/UsuarioRepository.java
@@ -3,11 +3,9 @@ package com.portfolio.portfolioBackend.Security.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.portfolio.portfolioBackend.Security.Models.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByNombreUsuario(String nombreUsuario);
